74bb042373f28bb2f563c54158a5c06e5f3d81a6
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-07-14 10:03:41 +0530

    Change body color to white Fixed card button position

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,15 +13,14 @@ const Footer = () => {
                 <li><Link to="/contact">Contact Us</Link></li>
             </ul>
             <hr />
-            {/*  */}
             <div className="social-media-icons">
-                <a href="https://www.facebook.com/radissonblu/" target="_blank" rel="noreferrer"><i class="fa-brands fa-facebook"></i></a>
-                <a href="https://www.instagram.com/radissonblu/?hl=en" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
-                <a href="https://twitter.com/radissonblu?lang=en" target="_blank" rel="noreferrer"><i class="fa-brands fa-twitter"></i></a>
-                <a href="https://www.google.com/maps/place/Radisson+Blu+Pune+Hinjawadi/@18.583876,73.7352347,17z/data=!3m1!4b1!4m8!3m7!1s0x3bc2bbe691c8d803:0x1ac30e73f856d9b4!5m2!4m1!1i2!8m2!3d18.583871!4d73.7397194" target="_blank" rel="noreferrer" ><i class="fa-solid fa-location-dot"></i></a>
+                <a href="https://www.facebook.com/radissonblu/" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook"></i></a>
+                <a href="https://www.instagram.com/radissonblu/?hl=en" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
+                <a href="https://twitter.com/radissonblu?lang=en" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter"></i></a>
+                <a href="https://www.google.com/maps/place/Radisson+Blu+Pune+Hinjawadi/@18.583876,73.7352347,17z/data=!3m1!4b1!4m8!3m7!1s0x3bc2bbe691c8d803:0x1ac30e73f856d9b4!5m2!4m1!1i2!8m2!3d18.583871!4d73.7397194" target="_blank" rel="noreferrer" ><i className="fa-solid fa-location-dot"></i></a>
             </div>
             <div className="copyright-text">
-                <p>Copyright <i class="fa-regular fa-copyright"></i> The Royal Palace</p>
+                <p>Copyright <i className="fa-regular fa-copyright"></i> The Royal Palace</p>
             </div>
         </div>
     )
